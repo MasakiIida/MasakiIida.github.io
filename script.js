4df06d6707625e8b30b8ccd87f4018af2d4a04cf
MasakiIida <45781872+MasakiIida@users.noreply.github.com>
2020-01-30 16:40:26 +0900

    Upload

---
+++ script.js
@@ -0,0 +1,37 @@
+$(function(){
+    // ホバー時のフォントサイズ変更
+    $(".menu").hover(function(){
+        $(this).stop().animate({'font-size':'30px', opacity:0.8},500);
+        }, function(){
+        $(this).stop().animate({'font-size':'20px', opacity:1},500);
+        });
+    
+    // ホバー時の明るさ変更
+    $(".main-images").hover(function(){
+        $(this).stop().animate({opacity:0.8},500);
+        }, function(){
+        $(this).stop().animate({opacity:1},500);
+        });
+        
+    
+    // ホバー時のフォントカラー切り替え
+    $(".footer-li").hover(function(){
+        $(this).stop().animate({'border':'1px solid #fff', 'color':'#82cddd'},300);
+        }, function(){
+        $(this).stop().animate({'border':'1px solid #fff', 'color':'#fff'},300);
+        });
+    
+    // スムーススクロール
+    $(function(){
+        $('a[href^="#"]').click(function() {
+            var speed = 400;
+            var href= $(this).attr("href");
+            var target = $(href == "#" || href == "" ? 'html' : href);
+            var position = target.offset().top;
+            $('body,html').animate({scrollTop:position}, speed, 'swing');
+            return false;
+        });
+    });
+    
+});
+
