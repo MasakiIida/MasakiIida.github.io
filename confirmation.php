c11a25275e33b2af6b7f4a64395d969f785116b2
MasakiIida <45781872+MasakiIida@users.noreply.github.com>
2020-03-01 23:38:56 +0900

    update

--- confirmation.php
+++ confirmation.php
@@ -1,4 +1,8 @@
 <?php
+// 日本語対応
+mb_language("ja");
+mb_internal_encoding("UTF-8");
+
 $field_name = $_POST['name'];
 $field_email = $_POST['email'];
 $field_phone = $_POST['phone'];
@@ -9,12 +13,20 @@ $body_message = 'From: '.$field_name."\n";
 $body_message .= 'E-mail: '.$field_email."\n";
 $body_message .= 'Phone: '.$field_phone."\n";
 $body_message .= 'Message: '.$field_message;
+$from = "Masaki";
+$from_name = "Masaki";
+$from_mail = "iidamasaki@www3901.sakura.ne.jp";
 $headers = 'From: '.$field_email."\r\n";
 $headers .= 'Reply-To: '.$field_email."\r\n";
-$mail_status = mail($mail_to, $subject, $body_message, $headers);
+$headers .= "Content-Type: text/plain \r\n";
+$headers .= "Organization: " . $field_name . " \r\n";
+$headers .= "X-Sender: " . $field_email . " \r\n";
+$headers .= "X-Priority: 3 \r\n";
+
+$mail_status = mb_send_mail($mail_to, $subject, $body_message, $headers);
 if ($mail_status) { ?>
 <script language="javascript" type="text/javascript">
-    window.location = 'mailto.php';//(※送信後に移動するページ)
+    window.location = 'mailto.html';//(※送信後に移動するページ)
 </script>
 <?php
 }
